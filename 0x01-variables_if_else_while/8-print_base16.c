1c4282211fee7a5d98365a0558e95c846201241d
hanfaoui <hanfaouiyoussef@gamil.com>
2023-03-20 23:38:31 +0000

    push all

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+
+int main(void)
+{
+	int digit = 48; 
+
+	while (digit <= 102) 
+	{
+		putchar(digit);
+
+		
+		if (digit == 57)
+			digit += 39;
+		++digit;
+	}
+	putchar('\n');
+
+	return (0);
+}
